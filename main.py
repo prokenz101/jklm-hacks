7ee3124950bb37201522ae3bf849624f4482608a
prokenz101 <75413484+prokenz101@users.noreply.github.com>
2022-10-06 14:01:41 +0400

    put everything inside a try except
    this is so that the ugly KeyboardInterrupt error doesn't show up and instead its just a clean "Exiting..."

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from pyperclip import paste
 from pathlib import Path
 from random import sample
 
-# * F9 keyboard hotkey
+try:
+    words = Path("dictionary").read_text().split("\n")
 
     print("""
     ░░░░░██╗██╗░░██╗██╗░░░░░███╗░░░███╗░░░░░░██╗░░██╗░█████╗░░█████╗░██╗░░██╗░██████╗
@@ -20,44 +21,46 @@ from random import sample
     print("And Ctrl + C to quit.")
     print()
 
-def on_press(key):
-    try:
-        key.char
+    def on_press(key):
+        try:
+            key.char
 
-    except AttributeError:
-        if str(key) == "Key.f9":
+        except AttributeError:
+            if str(key) == "Key.f9":
 
-            sleep(0.2)
-            hotkey("ctrl", "a")
-            sleep(0.2)
-            hotkey("ctrl", "c")
+                sleep(0.2)
+                hotkey("ctrl", "a")
+                sleep(0.2)
+                hotkey("ctrl", "c")
 
-            sleep(0.2)
-            clipboard = paste()
-            print(f"Prompt: {clipboard}")
+                sleep(0.2)
+                clipboard = paste()
+                print(f"Prompt: {clipboard}")
 
-            valid_words = list(filter(lambda x: clipboard in x, words))
-            random_words = None
-            if len(valid_words) == 0:
-                print("No word found!")
-                print()
-                return
+                valid_words = list(filter(lambda x: clipboard in x, words))
+                random_words = None
+                if len(valid_words) == 0:
                     press("backspace")
+                    print("No word found!")
+                    print()
+                    return
 
-            elif len(valid_words) >= 10:
-                random_words = sample(valid_words, 10)
+                elif len(valid_words) >= 10:
+                    random_words = sample(valid_words, 10)
 
-            else:
-                random_words = sample(valid_words, len(valid_words))
+                else:
+                    random_words = sample(valid_words, len(valid_words))
 
-            result = max(random_words)
+                result = max(random_words)
 
-            print(f"Result: {result}")
-            typewrite(result, interval=0.02)
-            print()
+                print(f"Result: {result}")
+                typewrite(result, interval=0.02)
+                print()
 
 
-with keyboard.Listener(on_press=on_press) as listener:
-    listener.join()
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
+
+except KeyboardInterrupt:
     print()
     print("Exiting...")
